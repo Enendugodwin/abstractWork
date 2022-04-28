6d50277552df896dab2b7cc66eea29635d29a210
Enendugodwin <66306762+Enendugodwin@users.noreply.github.com>
2022-04-28 20:07:58 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+    backgroundImage: {
+      'room': "url('../Abstract/img/Interior-Design-Ideas-Deconstructing-a-Unique-Master-Bedroom-Set-1.jpg')",
+    },
+  },
+  plugins: [],
+}
